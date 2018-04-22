53e6a705d08258ea9f10bac1b65f1cb0707421a6
evansu <evan.pohsun.su@gmail.com>
2018-04-22 16:09:27 -0400

    Handle case where network is not available and race condition

--- app/src/main/java/net/sourceforge/opencamera/TargetObjectReceiver.java
+++ app/src/main/java/net/sourceforge/opencamera/TargetObjectReceiver.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import com.android.volley.NetworkResponse;
@@ -13,9 +15,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-
-
-
+import java.util.concurrent.atomic.AtomicBoolean;
 /**
  * Created by evansu on 4/11/18.
  * A singleton class that fetch target object list on request
@@ -24,24 +24,32 @@ import com.android.volley.toolbox.StringRequest;
 public class TargetObjectReceiver {
 	private static final String TAG = "TargetObjectReceiver";
 	private static TargetObjectReceiver mInstance;
-	private static final String url = "https://raw.githubusercontent.com/Po-Hsun-Su/CapraRawCameraSettings/master/settings.txt";
+	private static final String url = "https://raw.githubusercontent.com/cucapra/CapraRawCamera/master/target_object.txt";
 	private static Context mCtx;
 	private String settings;
 	private int refresh_counter = 0;
+	private static AtomicBoolean request_sent;
 	private TargetObjectReceiver(Context context){
 		mCtx = context;
 	}
+	private static ConnectivityManager cm;
 
 	public static synchronized TargetObjectReceiver getInstance(Context context){
 		if(mInstance == null) {
 			mInstance = new TargetObjectReceiver(context);
+			cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+			request_sent = new AtomicBoolean(false);
 		}
 		return mInstance;
 	}
 	public int drawTargets(MyApplicationInterface applicationInterface, Canvas canvas, Paint p, int location_x, int location_y, int ui_rotation) {
 		int height = 0;
 		refresh_counter += 1;
-		if (settings == null || refresh_counter > 360) {
+		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+		boolean isConnected = activeNetwork != null &&
+				activeNetwork.isConnectedOrConnecting();
+
+		if ((settings == null && !request_sent.get() && isConnected) || refresh_counter > 6000 ) {
 			refresh_counter = 0;
 			StringRequest tokenRequest = new StringRequest(Request.Method.GET, url,
 					new Response.Listener<String>() {
@@ -50,6 +58,7 @@ public class TargetObjectReceiver {
 							// Display the first 500 characters of the response string.
 							Log.d(TAG, "onResponse: " + response);
 							settings = response;
+							request_sent.set(false);
 						}
 					}, new Response.ErrorListener() {
 				@Override
@@ -61,16 +70,18 @@ public class TargetObjectReceiver {
 					} else {
 						Log.d(TAG, "onErrorResponse: networkResponse is null");
 					}
+					request_sent.set(false);
 				}
 			});
 			RequestQueue queue = VolleyQueue.getInstance(mCtx).getRequestQueue();
+			request_sent.set(true);
 			queue.add(tokenRequest);
+
 		}
 		if(settings != null) {
 			Rect bounds = new Rect();
 			p.getTextBounds(settings, 0, settings.length(), bounds);
 			height = applicationInterface.drawTextWithBackground(canvas, p, settings, Color.WHITE, Color.BLACK, location_x, location_y, MyApplicationInterface.Alignment.ALIGNMENT_TOP, null, true, bounds);
-
 
 		}
 		return height;
